0ede924dc8680e93fdfd3d946d84f71133385d8b
wkshum99 <wkshum99@msn.com>
2015-09-24 15:15:22 +0800

    juniper data cleansed

--- access_log/jp.py
+++ access_log/jp.py
@@ -1,55 +1,37 @@
-import helper as hp
+import pandas as pd
 
-def process_juniper_line(line):
-    result = []
-    line = hp.remove_duplicate_spaces(line).split(' ')
-    result.append(line[0] + ' ' + line[1] + ' ' + line[2])
-    result.append(line[3])
-    result.append(line[4] + ' ' + line[5])
-    result.append(line[6])
-    result.append(line[8] + ' ' + line[9])
-    for i in range(10, 13):
-        result.append(line[i])
-    result.append(line[14] + ' ' + line[15])    
-    result.append(line[16] + ' ' + line[17])
-    for i in range(18, 25):
-        result.append(line[i])
-    result.append(line[26] + ' ' + line[27]) 
-    result.append(line[28] + ' ' + line[29]) 
-    for i in range(30, len(line) - 4):
-        result.append(line[i])
-    result.append(line[32] + ' ' + line[33] + ' ' + line[34] + ' ' + line[35])
-    return result
-
-def read_syslog_juniper(filename, filter=None):
-    result = []
-    
-    if (filename)[-3:] == '.gz':
-        f = hp.open_gz(filename)
-    elif (filename)[-6:] == 'tar.gz':
-        f = hp.open_tar_gz(filename)
+def juniper_log_cleansing(x):
+    if x.find('=') > 0:
+        return x[x.find('=')+1:]
+    elif x.find(':') > 0:
+        return x[:x.find(':')]    
     else:
-        f = hp.open_txt(filename)
+        return x
 
-    f = f.split('\n')
-
-    # file santization (remove duplicate space characters)
-    for row in f:
-        if row != '':
-            row = row.rstrip('\n')
-            temp = hp.remove_duplicate_spaces(row).split(' ')
-
-            # convert first field back to datetime
-            #temp[0] = convert_ts(temp[0])
-            result.append(temp)
-            
-    f = ''
+def read_syslog_juniper(filename):
     
-    # convert list into pandas dataframe
-    df = pd.DataFrame(result)
-    # remove blank column
-    #df.drop(df[[7]], axis=1, inplace='TRUE')
-    # rename columns
-    #df.columns = ['time', 'duration', 'client_address', 'result_code', 'bytes', 'method', 'URL', 'hierarchy', 'type']
+    #if (filename)[-3:] == '.gz':
+    #    f = hp.open_gz(filename)
+    #elif (filename)[-6:] == 'tar.gz':
+    #    f = hp.open_tar_gz(filename)
+    #else:
+    #    f = hp.open_txt(filename)
+    
+    #f = f.split('\n')
+    
+    df = pd.read_table(filename, sep=' {1,}|"', engine='python', index_col=False, header=None, parse_dates=[[0, 1, 2], [9,10]])
+    
+    # restructure the dataframe
+    i = [0]
+    i.extend(range(3, 4))
+    i.extend([1])
+    i.extend(range(9, 13))
+    i.extend([14])
+    i.extend([16])
+    i.extend(range(17, 25))
+    df = df.iloc[:, i]
+    
+    df.columns = ['time', 'device_name', 'traffic_start', 'duration', 'policy_no', 'service', 'protocol', 'src_zone', 'dst_zone', 'action', 'sent_size', 'received_size', 'src_address', 'dst_address', 'src_port', 'dst_port', 'remarks']
+    df[['device_name', 'duration', 'policy_no', 'service', 'protocol', 'src_zone', 'dst_zone', 'action', 'sent_size', 'received_size', 'src_address', 'dst_address', 'src_port', 'dst_port']] = df[['device_name', 'duration', 'policy_no', 'service', 'protocol', 'src_zone', 'dst_zone', 'action', 'sent_size', 'received_size', 'src_address', 'dst_address', 'src_port', 'dst_port']].applymap(juniper_log_cleansing)
     
     return df
