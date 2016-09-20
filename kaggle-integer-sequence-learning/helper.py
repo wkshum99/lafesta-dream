6b6ba7c2598e1235b17291a4139cbb61af872acc
wkshum99 <wkshum99@msn.com>
2016-09-20 16:40:00 +0800

    new folder

---
+++ kaggle-integer-sequence-learning/helper.py
@@ -0,0 +1,61 @@
+#import string
+#import datetime
+import pandas as pd
+#import numpy as np
+import gzip
+import tarfile
+import zipfile
+import zlib
+
+def open_gz(filename):
+    with gzip.open(filename, 'r') as f:
+        file = f.read()
+	return file
+
+def open_zip(file):
+    if zipfile.is_zipfile(file):
+        with zipfile.ZipFile(file, 'r') as f:
+            for i in f.namelist():
+                return (f.read(i))
+    else:
+        return False
+
+def open_tar_gz(filename):
+    file = []
+    with tarfile.open(filename, 'r:gz') as f:
+        for i in f.getmembers():
+            t= f.extractfile(i)
+            file.append(t.read())
+	return file
+
+def open_txt(filename):
+    with open(filename, 'r') as f:
+        file= f.read()
+	return file
+
+def open_csv(filename, chunksize=20000):
+    
+    total_chunksize = 0
+    
+    df = pd.DataFrame()
+    
+    for c in pd.read_table(filename, sep=' {1,}|"', chunksize=chunksize, iterator=True, engine='python', index_col=False, header=None, parse_dates=[[0, 1, 2], [9,10]]):
+    #for c in pd.read_table(filename, sep=':{1,}|=', chunksize=chunksize, iterator=True, engine='python', index_col=False, header=None, parse_dates=[[0, 1, 2], [9,10]]):    
+        df = pd.concat([df, c])
+        total_chunksize += chunksize
+        
+    print(str(total_chunksize) + " rows processed.")
+    
+    return df
+
+def zip_file(output, infile):
+    with zipfile.ZipFile(output, 'w') as zp:
+        zp.write(infile, compress_type=zipfile.ZIP_DEFLATED)
+
+#def remove_duplicate_spaces(word):
+#    #return ''.join(ch for ch, _ in itertools.groupby(word))
+#    return string.join(word.split())
+    
+#def convert_ts(timestamp):
+#    time = datetime.datetime.fromtimestamp(float(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+#    return time
